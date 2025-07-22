af57a63a2c73097c0e026f3af7277619b2ec77e3
bitcoincloudmining <bitcoincloudminingindia@gmail.com>
2025-07-22 21:33:57 +0530

    Updated auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,12 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const router = express.Router();
+require('dotenv').config();
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
     function (accessToken, refreshToken, profile, done) {
         // In production, save/find user in DB here
